6f25ee715c8a777d7ff83799fe7c818ece6efd71
Shri Har Balaji Iyyappan <balaji747bj@gmail.com>
2026-06-18 14:10:09 +0530

    Add files via upload

---
+++ diamond-star.py
@@ -0,0 +1,13 @@
+n = int(input("Enter the Number: "))
+for i in range(0, n):
+    for j in range(0, n-i-1):
+        print(" ",end="")
+    for j in range(0, i*2+1):
+        print("*", end="")
+    print()
+for i in range(1, n):
+    for j in range(0, i):
+        print(" ",end="")
+    for j in range(0, (n-i)*2-1):
+        print("*", end="")
+    print()
